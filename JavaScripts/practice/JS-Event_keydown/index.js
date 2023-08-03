7494835489d20d431f5c5669aee119cc52e1b8d4
dizas9 <sazid.ice07@gmail.com>
2023-08-03 22:52:01 +0600

    Create function for button 'Show' and 'Reset' 3/8/23

--- JavaScripts/practice/JS-Event_keydown/index.js
+++ JavaScripts/practice/JS-Event_keydown/index.js
@@ -1,7 +1,25 @@
-const inputText = document.getElementById('inputText');
-const output = document.getElementById('output');
+const inputText = document.getElementById("inputText");
+const output = document.getElementById("output");
+// variable to store pressed key as array
+const pressKeys = [];
 
-inputText.addEventListener('keydown', function(e){
+inputText.addEventListener("keydown", function (e) {
   const keypressed = e.key;
+  pressKeys.push(keypressed); // built in JS method used to add elemnt at the end of an array
   output.textContent = `${keypressed}`;
 });
+//'keydown' event doesnot fire for 'tab' key
+
+//A function to Show pressed key
+function ShowPressKeys() {
+  const keyString = pressKeys.join("");
+  window.alert(`You Pressed : ${keyString}`);
+}
+
+//function for reset button
+
+function ResetField(){
+  inputText.value = '';
+  pressKeys.length = 0;
+  output.textContent ='';
+}
